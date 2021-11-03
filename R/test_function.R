9acea6057a2844daa11936d56a3b3c4333936f29
Hampus Petren <hampus.petren@gmail.com>
2021-11-03 11:22:19 +0100

    edit description and add test-function manual

--- R/test_function.R
+++ R/test_function.R
@@ -1,1 +1,14 @@
-print("This is a test function")
+#' Test-function
+#'
+#' This is only a test function
+#'
+#' @param input Some text input
+#'
+#' @return Input and some more text
+#' @export
+#'
+#' @examples
+#' test_function("This input")
+test_function <- function(input) {
+  print(paste0("This was input: ", input))
+}
