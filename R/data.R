6047e0c7d1710c4e184aae3622e24d99045f67bc
Hampus Petren <hampus.petren@gmail.com>
2021-11-04 12:04:05 +0100

    add small alpina sample and compound dataset

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Arabis alpina floral scent data.
+#'
+#' A dataset with (proportional) floral scent data from three populations of
+#' the plant species Arabis alpina.
+#'
+#' @format A data frame with 87 rows and 16 columns. Each row is a sample,
+#' first column is population, the other columns are scent compounds.
+#'
+#' @source \url{https://doi.org/10.1093/aob/mcab007}
+"alpinaDataSubset"
+
+
+#' Arabis alpina floral scent compounds.
+#'
+#' A dataset listing the compounds found in alpinaDataSubset.
+#'
+#' @format A data frame with 15 rows and 3 columns. Each row is a compound.
+#' First column is a common name of the compound, second column is the
+#' SMILES (Simplified Molecular-Input Line-Entry System) specification,
+#' third column is the InChIKey (International Chemical Identifier).
+#'
+#' @source \url{https://doi.org/10.1093/aob/mcab007}
+"alpinaCompSubset"
