ecd397d306ff1ebefbee545ae0d438cc5cc4af00
Hampus Petren <hampus.petren@gmail.com>
2021-11-08 11:34:28 +0100

    add and document minimal dataset

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Arabis alpina floral scent data.
+#' Arabis alpina floral scent data
 #'
 #' A dataset with (proportional) floral scent data from three populations of
 #' the plant species Arabis alpina.
@@ -9,8 +9,7 @@
 #' @source \url{https://doi.org/10.1093/aob/mcab007}
 "alpinaDataSubset"
 
-
-#' Arabis alpina floral scent compounds.
+#' Arabis alpina floral scent compounds
 #'
 #' A dataset listing the compounds found in alpinaDataSubset.
 #'
@@ -21,3 +20,23 @@
 #'
 #' @source \url{https://doi.org/10.1093/aob/mcab007}
 "alpinaCompSubset"
+
+#' Minimal sample dataset
+#'
+#' A tiny made up dataset with floral scent data.
+#'
+#' @format A data frame with 4 rows and 4 columns. Each row is a sample,
+#' first column is group, the other columns are scent compounds.
+#'
+"minimalSampData"
+
+#' Minimal compound dataset
+#'
+#' A dataset with three floral scent compounds.
+#'
+#' @format A data frame with 3 rows and 3 columns. Each row is a compound.
+#' First column is a common name of the compound, second column is the
+#' SMILES (Simplified Molecular-Input Line-Entry System) specification,
+#' third column is the InChIKey (International Chemical Identifier).
+#'
+"minimalCompData"
