420f321f8d340909a05bbe5a50c1fc37277f2d63
Hampus Petren <hampus.petren@gmail.com>
2021-11-12 11:15:23 +0100

    add chemDivPlot function, manual, test

---
+++ tests/testthat/test-chemDivPlot.R
@@ -0,0 +1,30 @@
+testSampData <- data.frame(compA = c(0.3,0.4,0.1,0.2),
+                           compB = c(0.4,0.3,0.4,0.4),
+                           compC = c(0.3,0.3,0.5,0.4))
+
+testCompDis <- matrix(data = c(0,0.6,0.7,0.6,0,0.3,0.7,0.3,0), nrow = 3)
+colnames(testCompDis) <- c("compA", "compB", "compC")
+rownames(testCompDis) <- c("compA", "compB", "compC")
+
+testSampDis <- sampleDis(sampleData = testSampData)
+
+testDiv <- calcDiv(testSampData)
+
+testDivProf <- calcDivProf(testSampData)
+
+groups <- c("A","A","B","B")
+
+testChemDivPlot <- chemDivPlot(compDisMat = testCompDis,
+                               divData = testDiv,
+                               divProfData = testDivProf,
+                               sampleDisMat = testSampDis,
+                               groupData = groups)
+
+
+test_that("chemodiversity plots are outputted", {
+  expect_match(typeof(testChemDivPlot), "list")
+  expect_equal(nrow(testChemDivPlot), 2)
+  expect_equal(ncol(testChemDivPlot), 2)
+})
+
+
