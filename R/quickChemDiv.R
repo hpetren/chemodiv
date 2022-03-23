3416060bb0acaed36428ea2426ab828d6e79b01f
Hampus Petren <hampus.petren@gmail.com>
2022-03-23 11:06:58 +0100

    make quickChemDiv work with/without compound data

--- R/quickChemDiv.R
+++ R/quickChemDiv.R
@@ -5,19 +5,24 @@
 #' requested also visualized, for users wanting to quickly explore their
 #' data using standard parameters.
 #'
-#' The function takes compound data and sample data as input. See
-#' \code{\link{chemdiv}} for details on data formatting. This function then
-#' uses the following other functions in the package:
+#' The function requires sample data as input, and can also include
+#' compound data. If only sample data is supplied, phytochemical diversity and
+#' dissimilarity will be calculated as Hill diversity and Bray-Curtis
+#' dissimilarities, respectively. If sample data nd compound data is supplied,
+#' phytochemical diversity and dissimilarity will be calculated as
+#' Functional Hill diversity and Generalized UniFrac dissimilarities,
+#' respectively. See \code{\link{chemdiv}} for details on data formatting.
+#' This function then uses the following other functions in the package:
 #' \itemize{
 #' \item \code{\link{compDis}} is used to calculate compound dissimilarities
-#' using PubChem Fingerprints.
-#' \item \code{\link{calcDiv}} is used to calculate Functional Hill Diversity
-#' for q = 1.
+#' using PubChem Fingerprints, if compound data is supplied.
+#' \item \code{\link{calcDiv}} is used to calculate (Functional) Hill
+#' Diversity for q = 1.
 #' \item \code{\link{calcDivProf}} is used to calculate a diversity profile
-#' with Functional Hill Diversity for q = 0-3.
-#' \item \code{\link{sampleDis}} is used to calculate Generalized UniFrac
-#' dissimilarities between samples.
-#' \item \code{\link{chemDivPlot}} is used to create four different
+#' with (Functional) Hill Diversity for q = 0-3.
+#' \item \code{\link{sampleDis}} is used to calculate Bray-Curtis/Generalized
+#' UniFrac dissimilarities between samples.
+#' \item \code{\link{chemDivPlot}} is used to create different
 #' chemodiversity plots if requested.
 #' }
 #'
@@ -27,24 +32,25 @@
 #' However, for detailed analyses we recommend using the separate
 #' functions to allow for full control of function parameters and output.
 #'
+#' @param sampleData Data frame with the relative concentration of each
+#' compound (column) in every sample (row).
 #' @param compoundData Data frame with the chemical compounds of interest.
 #' Should have a column named "compound" with common names, a column named
 #' "smiles" with SMILES IDs for the compounds, and a column named "inchikey"
 #' with the InChIKey IDs for the compounds. See \code{\link{chemdiv}} for
 #' details on obtaining SMILES and InChIKey IDs.
-#' @param sampleData Data frame with the relative concentration of each
-#' compound (column) in every sample (row).
 #' @param groupData Grouping data.
 #' @param outputType Type of output that should be returned: either
 #' \code{"data"} to output a list with different types of chemodiversity data,
 #' or \code{"plots"} to instead produce standard plots of this data.
 #'
-#' @return Four types of chemodiversity measures, as either elements in a list
-#' or separate plots. If \code{outputType = "data"}, function returns
-#' a compound dissimilarity matrix, a data frame with Functional Hill
-#' Diversity at q = 1, a data frame with a Functional Hill Diversity profile
-#' for q = 0-3, and a sample dissimilarity matrix. If
-#' \code{outputType = "plots"}, these data sets are plotted as a dendrogram,
+#' @return Various types of chemodiversity measures, either as elements in
+#' a list or as separate plots. If \code{outputType = "data"}, function returns
+#' a compound dissimilarity matrix (if compound data was supplied),
+#' a data frame with (Functional) Hill Diversity at q = 1,
+#' a data frame with a (Functional) Hill Diversity profile for q = 0-3,
+#' and a sample dissimilarity matrix. If \code{outputType = "plots"},
+#' these data sets are plotted as a dendrogram (if compound data was supplied),
 #' a boxplot, a diversity profile plot and an NMDS plot, respectively.
 #'
 #' @export
@@ -53,10 +59,10 @@
 #' data(minimalCompData)
 #' data(minimalSampData)
 #' groups <- c("A", "A", "B", "B")
-#' quickChemDiv(compoundData = minimalCompData, sampleData = minimalSampData,
+#' quickChemDiv(sampleData = minimalSampData, compoundData = minimalCompData,
 #' groupData = groups, outputType = "plots")
-quickChemDiv <- function(compoundData,
-                         sampleData,
+quickChemDiv <- function(sampleData,
+                         compoundData = NULL,
                          groupData = NULL,
                          outputType = "plots") {
 
@@ -65,38 +71,66 @@ quickChemDiv <- function(compoundData,
     stop('outputType must be either "data" or "plots".')
   }
 
-  quickCompDis <- compDis(compoundData = compoundData,
-                          type = "PubChemFingerprint",
-                          npcTable = NULL,
-                          unknownCompoundsMean = FALSE)
+  if (is.null(compoundData)) {
 
-  quickDiv <- calcDiv(sampleData = sampleData,
-                      compDisMat = quickCompDis$fingerDisMat,
-                      type = "FuncHillDiv",
-                      q = 1)
+    quickDiv <- calcDiv(sampleData = sampleData,
+                        type = "HillDiv",
+                        q = 1)
 
-  quickDivProf <- calcDivProf(sampleData = sampleData,
-                              compDisMat = quickCompDis$fingerDisMat,
-                              type = "FuncHillDiv",
-                              qMin = 0,
-                              qMax = 3,
-                              step = 0.1)
+    quickDivProf <- calcDivProf(sampleData = sampleData,
+                                type = "HillDiv",
+                                qMin = 0,
+                                qMax = 3,
+                                step = 0.1)
 
-  quickSampleDis <- sampleDis(sampleData = sampleData,
-                              compDisMat = quickCompDis$fingerDisMat,
-                              type = "GenUniFrac",
-                              alpha = 1)
+    quickSampleDis <- sampleDis(sampleData = sampleData,
+                                type = "BrayCurtis")
 
-  if (outputType == "data") {
-    chemDivData <- list(compoundDissimilarity = quickCompDis$fingerDisMat,
-                        funcHillDiv = quickDiv,
-                        funcHillDivProf = quickDivProf$divProf,
-                        sampleDissimilarity = quickSampleDis$GenUniFrac)
+    if (outputType == "data") {
+      chemDivData <- list(HillDiv = quickDiv,
+                          HillDivProf = quickDivProf$divProf,
+                          SampleDissimilarity = quickSampleDis$BrayCurtis)
+    } else {
+      chemDivPlot(divData = quickDiv,
+                  divProfData = quickDivProf,
+                  sampleDisMat = quickSampleDis$BrayCurtis,
+                  groupData = groupData)
+    }
+
   } else {
-    chemDivPlot(compDisMat = quickCompDis$fingerDisMat,
-                divData = quickDiv,
-                divProfData = quickDivProf,
-                sampleDisMat = quickSampleDis$GenUniFrac,
-                groupData = groupData)
+    quickCompDis <- compDis(compoundData = compoundData,
+                            type = "PubChemFingerprint",
+                            npcTable = NULL,
+                            unknownCompoundsMean = FALSE)
+
+    quickDiv <- calcDiv(sampleData = sampleData,
+                        compDisMat = quickCompDis$fingerDisMat,
+                        type = "FuncHillDiv",
+                        q = 1)
+
+    quickDivProf <- calcDivProf(sampleData = sampleData,
+                                compDisMat = quickCompDis$fingerDisMat,
+                                type = "FuncHillDiv",
+                                qMin = 0,
+                                qMax = 3,
+                                step = 0.1)
+
+    quickSampleDis <- sampleDis(sampleData = sampleData,
+                                compDisMat = quickCompDis$fingerDisMat,
+                                type = "GenUniFrac",
+                                alpha = 1)
+
+    if (outputType == "data") {
+      chemDivData <- list(CompoundDissimilarity = quickCompDis$fingerDisMat,
+                          FuncHillDiv = quickDiv,
+                          FuncHillDivProf = quickDivProf$divProf,
+                          SampleDissimilarity = quickSampleDis$GenUniFrac)
+    } else {
+      chemDivPlot(compDisMat = quickCompDis$fingerDisMat,
+                  divData = quickDiv,
+                  divProfData = quickDivProf,
+                  sampleDisMat = quickSampleDis$GenUniFrac,
+                  groupData = groupData)
+    }
   }
 }
--- tests/testthat/test-quickChemDiv.R
+++ tests/testthat/test-quickChemDiv.R
@@ -1,33 +1,22 @@
-testCompData <- data.frame(compounds = c("limonene",
-                                         "benzaldehyde",
-                                         "Unknown1"),
-                           smiles = c("CC1=CCC(CC1)C(=C)C",
-                                      "C1=CC=C(C=C1)C=O",
-                                      NA),
-                           inchikey = c("XMGQYMWWDOXHJM-UHFFFAOYSA-N",
-                                        "HUMNYLRZRPPJDN-UHFFFAOYSA-N",
-                                        NA))
 testSampData <- data.frame(limonene = c(0.3,0.4,0.1,0.2),
                            benzaldehyde = c(0.4,0.3,0.4,0.4),
                            Unknown1 = c(0.3,0.3,0.5,0.4))
 groups <- c("I","I","II","II")
 
-testQuickChemDiv1 <- quickChemDiv(compoundData = testCompData,
-                                  sampleData = testSampData,
-                                  groupData = groups,
+# Only testing without compound data
+testQuickChemDiv1 <- quickChemDiv(sampleData = testSampData,
                                   output = "data")
 
-testQuickChemDiv2 <- quickChemDiv(compoundData = testCompData,
-                                  sampleData = testSampData,
-                                  groupData = groups,
-                                  output = "plots")
+testQuickChemDiv2 <- suppressWarnings(quickChemDiv(sampleData = testSampData,
+                                                   groupData = groups,
+                                                   output = "plots"))
 
-test_that("quichChemDiv works with data output", {
+test_that("quickChemDiv works with data output", {
   expect_match(typeof(testQuickChemDiv1), "list")
-  expect_equal(length(testQuickChemDiv1), 4)
+  expect_equal(length(testQuickChemDiv1), 3)
 })
 
-test_that("quichChemDiv works with plot output", {
+test_that("quickChemDiv works with plot output", {
   expect_match(typeof(testQuickChemDiv2), "list")
   expect_equal(nrow(testQuickChemDiv2), 2)
   expect_equal(ncol(testQuickChemDiv2), 2)
