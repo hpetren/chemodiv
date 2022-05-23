4e6266023ea8f33dfe837ace4ae52cfd90eede37
Hampus Petren <hampus.petren@gmail.com>
2022-05-23 14:26:47 +0200

    update NPCTable/compDis tests

--- tests/testthat/test-NPCTable.R
+++ tests/testthat/test-NPCTable.R
@@ -1,13 +1,20 @@
-testCompData <- data.frame(compound = c("limonene", "unknown"),
-                           smiles = c("CC1=CCC(CC1)C(=C)C", "NOTSMILES"),
-                           inchikey = c("XMGQYMWWDOXHJM-UHFFFAOYSA-N",
+testCompData <- data.frame(compound = c("limonene", "unknown", "faulty"),
+                           smiles = c("CC1=CCC(CC1)C(=C)C", NA, "NOTSMILES"),
+                           inchikey = c("XMGQYMWWDOXHJM-UHFFFAOYSA-N", NA,
                                         "NOTINCHIKEY"))
 
+# Skip test that uses internet resources
 test_that("NPC-classification is generated", {
+  skip_on_cran()
+  skip_if_offline()
   expect_equal(nrow(NPCTable(testCompData[1,])), nrow(testCompData[1,]))
   expect_message(NPCTable(testCompData), "Is the SMILES correct?")
 })
 
+# Skip test that uses internet resources
 test_that("warnings and messages work", {
+  skip_on_cran()
+  skip_if_offline()
   expect_error(NPCTable(testCompData[2,]))
+  expect_error(NPCTable(testCompData[3,]))
 })
--- tests/testthat/test-compDis.R
+++ tests/testthat/test-compDis.R
@@ -1,24 +1,46 @@
 testCompData <- data.frame(compound = c("limonene",
                                         "benzaldehyde",
-                                        "Unknown1"),
+                                        "Unknown1",
+                                        "faulty"),
                            smiles = c("CC1=CCC(CC1)C(=C)C",
                                       "C1=CC=C(C=C1)C=O",
-                                      NA),
+                                      NA,
+                                      "NOTSMILES"),
                            inchikey = c("XMGQYMWWDOXHJM-UHFFFAOYSA-N",
                                         "HUMNYLRZRPPJDN-UHFFFAOYSA-N",
-                                        NA))
+                                        NA,
+                                        "NOTINCHIKEY"))
 
-compDisRes <- suppressWarnings(compDis(testCompData,
-                                       type = c("NPClassifier",
-                                                "PubChemFingerprint",
-                                                "fMCS")))
+testNpcTable <- data.frame(compound = c("limonene",
+                                        "benzaldehyde"),
+                           smiles = c("CC1=CCC(CC1)C(=C)C",
+                                      "C1=CC=C(C=C1)C=O"),
+                           inchikey = c("XMGQYMWWDOXHJM-UHFFFAOYSA-N",
+                                        "HUMNYLRZRPPJDN-UHFFFAOYSA-N"),
+                           pathway = c("Terpenoids",
+                                       "Shikimates and Phenylpropanoids"),
+                           superclass = c("Monoterpenoids",NA),
+                           class = c("Menthane monoterpenoids",NA),
+                           class2 = c("Monocyclic monoterpenoids",NA))
 
+# Skip test that uses internet resources
 test_that("all three types of compDis and their mean works", {
-  expect_output(str(compDisRes), "List of 4")
-  expect_equal(ncol(compDisRes$meanDisMat), 3)
-  expect_equal(nrow(compDisRes$meanDisMat), 3)
+  skip_on_cran()
+  skip_if_offline()
+  expect_output(str(suppressWarnings(compDis(testCompData,
+                                             type = c("NPClassifier",
+                                                      "PubChemFingerprint",
+                                                      "fMCS")))),
+                "List of 4")
 })
 
 test_that("function stops if no type is correct", {
   expect_error(compDis(testCompData, type = "wrong"))
+})
+
+test_that("function works with NPClassifier and table", {
+  expect_output(str(suppressWarnings(compDis(testCompData[1:2,],
+                                             type = "NPClassifier",
+                                             npcTable = testNpcTable))),
+                "List of 1")
 })
